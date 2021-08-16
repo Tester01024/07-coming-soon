80f842c3a1320b4614bbd391d06550bb5439ff2c
Tester01024 <auziebijus@gmail.com>
2021-08-16 20:16:55 +0300

    Import / exprt

---
+++ css/js/components/progress-bar/ProgressBars.js
@@ -0,0 +1,9 @@
+// skirtas konstruoti logikai
+// minciu destymo sistema
+class ProgressBars {
+    constructor() {
+        console.log('running...');
+    }
+}
+
+export { ProgressBars }
---
+++ css/js/main.js
@@ -0,0 +1,5 @@
+// vieta kur bus ishkviesta visa susijusi logika
+
+import { ProgressBars } from "./components/progress-bar/ProgressBars.js";
+// new ProgressBars()
+// new ProgressBars()
